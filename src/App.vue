fe59154fb9aeee0122b938d3bf507a7a75f64d58
Artem Zatula <artemzatula@lwo-vl0020-148.corp.dtm.io>
2018-10-30 19:12:19 +0200

    Added new components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Form msg="Artem"/>
+    <ToDoItem name="Daryna"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
+import ToDoItem from './components/ToDoItem.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Form,
+    ToDoItem
   }
 }
 </script>
