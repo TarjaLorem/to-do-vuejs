9087a2ce7d0faf70f27776ee3604dde8a2968610
Darya Ulyanova <daryaulyanova@lwo-vl0020-182.corp.dtm.io>
2018-10-31 18:04:53 +0200

    cleaned

--- src/components/AddTodoItem.vue
+++ src/components/AddTodoItem.vue
@@ -1,36 +1,12 @@
 <template>
-  <div class="add-todo-item">
-    <input :value="newItem" placeholder="Add some item" type="text" class="add add-input">
-    <button @click="addItem" class="add">Add Todo</button>
-    <div class="list-group-item" :key="todo.title" v-for="todo in todos">{{todo.title}}</div>
-  </div>
+  <div class="add-todo-item"></div>
 </template>
 
 <script>
 export default {
   name: 'add-todo-item',
-  computed: {
-     newItem(){
-      return this.$store.getters.newItem;
-    },
-    todos() {
-      return this.$store.state.todos;
-    }
-  },
-  methods: {
-    // addItem() {
-    //   this.$store.state.todos.push({
-    //     id: this.idItem,
-    //     title: this.newItem,
-    //     completed: false
-    //   });
-    //   this.newItem = '';
-    //   this.idItem++;
-    // },
-    addItem(){
-      this.$store.dispatch('addTodo')
-    }
-  }
+  computed: {},
+  methods: {}
 }
 </script>
 
@@ -48,17 +24,5 @@ li {
 }
 a {
   color: #42b983;
-}
-.add-todo-item {
-  display: flex;
-  justify-content: center;
-}
-.add {
-  width: 300px;
-  padding: 15px;
-  font-size: 16px;
-}
-.add-input {
-  margin-right: 20px;
 }
 </style>
