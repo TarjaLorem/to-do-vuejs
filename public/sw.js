e677ed54785f5ee510372f6e746dfdd593412b2a
Darya Ulyanova <daryaulyanova@lwo-vl0020-39.corp.dtm.io>
2018-11-05 10:46:15 +0200

    refactored the code

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,6 @@ const CACHE_NAME = "pwa-todo-v1";
 const filesToCache = [
   "./",
   "./index.html",
-  "https://cdn.jsdelivr.net/npm/vue",
   "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
 ];
 
