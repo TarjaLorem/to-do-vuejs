28412e248e38e0c3ff23fb34e00d8d019eea36d9
Darya Ulyanova <daryaulyanova@lwo-vl0020-61.corp.dtm.io>
2018-11-06 18:10:57 +0200

    deploy

---
+++ precache-manifest.a792021b2cd592250de7d93b680f349e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1748b78080b402893208ead84326c0c",
+    "url": "/to-do-vuejs/service-worker.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/to-do-vuejs/robots.txt"
+  },
+  {
+    "revision": "da4f6997565a913cd90a",
+    "url": "/to-do-vuejs/js/chunk-vendors.ee36946f.js"
+  },
+  {
+    "revision": "ba60a2b9a94fc2ac04d7",
+    "url": "/to-do-vuejs/js/app.f5a277df.js"
+  },
+  {
+    "revision": "63a9dadfce68cfc2680e085bbc4ffb94",
+    "url": "/to-do-vuejs/index.html"
+  },
+  {
+    "revision": "da4f6997565a913cd90a",
+    "url": "/to-do-vuejs/css/chunk-vendors.323f5a38.css"
+  },
+  {
+    "revision": "ba60a2b9a94fc2ac04d7",
+    "url": "/to-do-vuejs/css/app.6f7c047f.css"
+  }
+];
